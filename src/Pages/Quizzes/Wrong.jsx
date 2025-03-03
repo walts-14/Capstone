2ca2ebef0088ae98e64174b04cae0fa9e122f293
henry2912v <164138976+henry2912v@users.noreply.github.com>
2025-03-03 17:06:21 +0800

    directory changes in quiz

--- src/Pages/Quizzes/Wrong.jsx
+++ src/Pages/Quizzes/Wrong.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
-import "../css/Correct.css";
+import "../../css/Wrong.css";
 import { useNavigate } from "react-router-dom";
-import backkpoint from "../assets/backkpoint.png";
-import arrow from "../assets/arrow.png";
-import check from "../assets/check.png";
+import backkpoint from "../../assets/backkpoint.png";
+import arrow from "../../assets/arrow.png";
+import ekis from "../../assets/ekis.png";
 import axios from "axios";
 
-function Correct() {
+function Wrong() {
   const [quiz, setQuiz] = useState({ question: "Sample Question?" });
   const [selectedAnswer, setSelectedAnswer] = useState(null);
   const [result, setResult] = useState(null);
@@ -51,49 +51,48 @@ function Correct() {
         <p>Back</p>
       </div>
 
-      <div className="quiz-container d-flex fw-bold">
+      <div className="quiz-container fw-bold">
         <p className="quiz-question">{quiz.question}</p>
         {/* Display question only */}
       </div>
-
-      <div className="gridd text-center fw-bold rounded-4">
-        <div className="picture-question d-flex ms-auto rounded-4"></div>
-        <div className="correct-ans d-flex text-center justify-content-left ps-5 pt-3 fs-3">
-          Correct Answer
-          <img src={check} class="img-fluid p-1 mt-1" alt="check img" />
+      <div className="grids text-center fw-bold rounded-4">
+        <div className="picturee-question d-flex rounded-4"></div>
+        <div className="wrong-ans d-flex text-center justify-content-left ps-5 pt-3 fs-3">
+          Wrong Answer
+          <img src={ekis} class="img-fluid p-1 mt-1" alt="wrong img" />
         </div>
-        <div className="choicess rounded-4 col-md-6 col-lg-11 m-5">
-          <div className="choicee-a rounded-4 m-4">a</div>
-        </div>
-
-        <div className="choicess rounded-4 col-md-6 col-lg-11 m-5">
-          <div className="choicee-b rounded-4 m-4">b</div>
+        <div className="choicesss rounded-4 col-md-6 col-lg-11 m-5">
+          <div className="choiceee-a rounded-4 m-4">a</div>
         </div>
 
-        <div className="choicess rounded-4 col-md-6 col-lg-11 m-5">
-          <div className="choicee-c rounded-4 m-4">c</div>
+        <div className="choicesss rounded-4 col-md-6 col-lg-11 m-5">
+          <div className="choiceee-b rounded-4 m-4">b</div>
         </div>
 
-        <div className="choicess rounded-4 col-md-6 col-lg-11 m-5">
-          <div className="choicee-d rounded-4 m-4">d</div>
+        <div className="choicesss rounded-4 col-md-6 col-lg-11 m-5">
+          <div className="choiceee-c rounded-4 m-4">c</div>
+        </div>
+
+        <div className="choicesss rounded-4 col-md-6 col-lg-11 m-5">
+          <div className="choiceee-d rounded-4 m-4">d</div>
         </div>
       </div>
 
       <button
         type="button"
-        className="continuee d-flex rounded-4 p-3 pt-2 ms-auto"
-        onClick={() => navigate("/wrong")}
+        className="continueee d-flex rounded-4 p-3 pt-2 ms-auto"
+        onClick={() => navigate("/repeat")}
       >
         Next
         <img
           src={arrow}
           class="img-fluid d-flex ms-auto p-1 mt-1  "
           alt="arrow img"
-          onClick={() => navigate("/wrong")}
+          onClick={() => navigate("/repeat")}
         />
       </button>
     </>
   );
 }
 
-export default Correct;
+export default Wrong;
