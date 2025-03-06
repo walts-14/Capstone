c65b3b15c0971a72bf68d27b1a07db963eb04b96
henry2912v <164138976+henry2912v@users.noreply.github.com>
2025-03-06 14:46:02 +0800

    add sidenav  stay highlight when click

--- src/Components/Sidenav.jsx
+++ src/Components/Sidenav.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import "../css/Dashboard.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function Sidenav() {
+  const location = useLocation();
+
   return (
     <>
       <div className="logo fw-bold">
@@ -10,22 +13,38 @@ function Sidenav() {
       </div>
       <div className="navbar-bg d-flex p-5">
         <nav className="sideNav d-flex flex-column fs-2 ">
-          <div className="dashboard">
-            <a className="nav-link mb-5" aria-current="page" href="/dashboard">
+          <div
+            className={`dashboard ${
+              location.pathname === "/dashboard" ? "active" : ""
+            }`}
+          >
+            <a className="nav-link mb-5" href="/dashboard">
               Dashboard
             </a>
           </div>
-          <div className="library mt-4 p-2 pt-4">
+          <div
+            className={`library mt-4 p-2 pt-4 ${
+              location.pathname === "/Library" ? "active" : ""
+            }`}
+          >
             <a className="nav-link mb-5" href="/Library">
               Library
             </a>
           </div>
-          <div className="leaderboard mt-4">
+          <div
+            className={`leaderboard mt-4 ${
+              location.pathname === "/leaderboard" ? "active" : ""
+            }`}
+          >
             <a className="nav-link mb-5" href="/leaderboard">
               Leaderboard
             </a>
           </div>
-          <div className="settings mt-4">
+          <div
+            className={`settings mt-4 ${
+              location.pathname === "/settings" ? "active" : ""
+            }`}
+          >
             <a className="nav-link mb-5" href="/settings">
               Settings
             </a>
@@ -35,4 +54,5 @@ function Sidenav() {
     </>
   );
 }
+
 export default Sidenav;
