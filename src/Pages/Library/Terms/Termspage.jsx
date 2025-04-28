36ec23a8167aa33bdd4488f43c23fb8b93a62e88
walts-14 <walterjohne14@gmail.com>
2025-04-28 11:52:02 +0800

    lessontitle

--- src/Pages/Library/Terms/Termspage.jsx
+++ src/Pages/Library/Terms/Termspage.jsx
@@ -4,12 +4,14 @@ import { useParams } from "react-router-dom";
 import Termslist from "./Termslist";
 import Sidenav from "../../../Components/Sidenav";
 import LibraryButtons from "../LibraryButtons";
+import Lessons from "../../../Components/dataLessons";
+import Lessonlist from "../../../Components/Lessonlist";
 
 function Termspage() {
   const { termId } = useParams(); // e.g., "termsone", "termstwo", etc.
   const [terms, setTerms] = useState([]);
   const [loading, setLoading] = useState(true);
-
+  const lesson = Lessons[termId]; // Get lesson details (dataLessons)
   useEffect(() => {
     fetch(`http://localhost:5000/api/videos${termId ? `?lessonKey=${termId}` : ""}`)
       .then((response) => response.json())
@@ -35,6 +37,7 @@ function Termspage() {
 
   return (
     <div className="termspage-content">
+      {lesson ? <Lessonlist Lessons={lesson} /> : <h1>No Data Found</h1>}
       <Sidenav />
       <LibraryButtons />
       <Termslist LessonTerms={terms} lessonKey={termId} />
@@ -43,3 +46,5 @@ function Termspage() {
 }
 
 export default Termspage;
+
+
