297ac63f7f19ed80592decff9968921073c48172
henry2912v <164138976+henry2912v@users.noreply.github.com>
2025-05-30 12:42:50 +0800

    install tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   css: {
-    postcss: './postcss.config.cjs', // Ensure this points to your PostCSS config
+    postcss: "./postcss.config.cjs", // Ensure this points to your PostCSS config
   },
   server: {
     proxy: {
