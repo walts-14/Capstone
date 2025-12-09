5b4b333600f52806b67e69563ba50f38d92bbd93
Doffy-cyber <187972018+Doffy-cyber@users.noreply.github.com>
2025-12-10 01:16:52 +0800

    video routes for uplaod

--- backend/routes/videoRoutes.js
+++ backend/routes/videoRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import upload from "../config/multer.js";
-import { uploadVideo, getVideos } from "../controllers/video.controller.js";
+import { protect } from "../middlewares/auth.js";
+import { uploadVideo as uploadVideoMiddleware} from "../config/multer.js";
+import { uploadVideo as uploadVideoController, getVideos } from "../controllers/video.controller.js";
 
 const router = express.Router();
 
 // POST /api/videos/upload - Upload video
-router.post("/upload", upload.single("file"), uploadVideo);
+router.post("/upload", protect, uploadVideoMiddleware.single("file"), uploadVideoController);
 
 // GET /api/videos - Retrieve videos with optional query parameters
 router.get("/", getVideos);
