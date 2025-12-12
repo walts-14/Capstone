240976dec51e4a6eb57a9e0731ca19d0d07af547
walts-14 <johnewalteralmcd@gmail.com>
2025-12-13 00:30:09 +0800

    howtoplay revise/points color change

--- src/Pages/Introduction/HowtoPlay.jsx
+++ src/Pages/Introduction/HowtoPlay.jsx
@@ -6,8 +6,9 @@ import how3 from "../../assets/how3.png";
 import how4 from "../../assets/how4.png";
 import how5 from "../../assets/how5.png";
 import how6 from "../../assets/how6.png";
+import how7 from "../../assets/how7.png";
 export default function HowtoPlay() {
-  const howImages = [how1, how5, how2, how3, how4, how6];
+  const howImages = [how1, how2, how3, how4, how5,how6, how7];
   return (
     <div className="howtoplay-container">
       {howImages.map((src, idx) => (
