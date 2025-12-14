c008ada9a335bc3ce477e60df1ee65ba8d82cc2c
henry2912v <momotunes2919@gmail.com>
2025-12-14 21:56:32 +0800

    minor change

--- src/Pages/Login/Admin/SuperAdmin.jsx
+++ src/Pages/Login/Admin/SuperAdmin.jsx
@@ -743,7 +743,7 @@ const SuperAdmin = () => {
                           borderRadius: "30px",
                           border: "2px solid #e0e0e0",
                           zIndex: 2000,
-                          width: "45%",
+                          width: "60%",
                           height: "80%",
                           boxShadow: "0 4px 24px rgba(0,0,0,0.18)",
                           padding: "2rem 2.5rem 2rem 2.5rem",
